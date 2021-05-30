4fea8aaead604dce46d71fa71f3c11e84e63279c
fernando <fe.jcorreia@gmail.com>
2021-05-30 16:07:06 -0300

    deploy bug string undefined fixed

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,6 +1,6 @@
 import Prismic from "@prismicio/client";
 
 export function getPrismicClient() {
-  const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT);
+  const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT as string);
   return prismic;
 }
